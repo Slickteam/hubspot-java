61b63acfb0891a385d4ab87520dc1415accace5d
Elodie <elodie.bellec@slickteam.fr>
2023-06-22 10:59:15 +0200

    #3 - JSon tests on pipeline and stage

---
+++ src/test/java/fr/slickteam/hubspot/api/unit/HSStageJSONTest.java
@@ -0,0 +1,43 @@
+package fr.slickteam.hubspot.api.unit;
+
+import fr.slickteam.hubspot.api.domain.HSStage;
+import fr.slickteam.hubspot.api.service.HSPipelineService;
+import fr.slickteam.hubspot.api.service.HubSpot;
+import fr.slickteam.hubspot.api.utils.Helper;
+import kong.unirest.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class HSStageJSONTest {
+
+     HSPipelineService service;
+
+    @Before
+    public void setUp() throws IOException {
+        service = new HubSpot(Helper.provideHubspotProperties()).pipeline();
+    }
+
+    @Test
+    public void parseStageData_Test() {
+        String inputData = "{test:1,id:71}";
+        JSONObject jsonObject = new JSONObject(inputData);
+        HSStage stage = service.parseStageData(jsonObject);
+
+        assertEquals(71, stage.getId());
+        assertEquals("1", stage.getProperty("test"));
+    }
+
+    @Test
+    public void toJson_Test() {
+        String inputData = "{test:1, test2:2,id:71}";
+        JSONObject jsonObject = new JSONObject(inputData);
+
+        HSStage stage = service.parseStageData(jsonObject);
+        String result = stage.toJson().toString();
+        assertEquals("{\"properties\":{\"test2\":\"2\",\"id\":\"71\",\"test\":\"1\"}}", result);
+    }
+}
