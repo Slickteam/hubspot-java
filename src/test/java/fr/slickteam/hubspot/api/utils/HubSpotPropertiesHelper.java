5b850e4bd8aad55994f37d0bcc2c607777383974
Ronan Barbot <ronan.barbot@slickteam.fr>
2023-11-08 08:57:26 +0100

    fix tests

--- src/test/java/fr/slickteam/hubspot/api/utils/HubSpotPropertiesHelper.java
+++ src/test/java/fr/slickteam/hubspot/api/utils/HubSpotPropertiesHelper.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class HubSpotPropertiesHelper {
+
     public static Properties loadProperties() throws IOException {
         Properties res = new Properties();
         res.load(HubSpotPropertiesHelper.class.getClassLoader().getResourceAsStream("config.properties"));
@@ -22,7 +23,7 @@ public class HubSpotPropertiesHelper {
 
     public static String loadPropertyValue(String propertyName) throws IOException {
         String valueFromFile = loadProperties().getProperty(propertyName);
-        if (valueFromFile == null || (valueFromFile.isBlank() && valueFromFile.equals("..."))) {
+        if (valueFromFile == null || (valueFromFile.isBlank() || valueFromFile.equals("..."))) {
             return System.getenv(propertyName);
         }
         return valueFromFile;
